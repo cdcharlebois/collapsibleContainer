a3f57085bc5d493860cdea5b6a6bbd9d6f55ab1a
Conner Charlebois <cdcharlebois@gmail.com>
2020-04-01 13:52:15 -0400

    first commit

---
+++ src/CollapsibleContainer.jsx
@@ -0,0 +1,35 @@
+import { Component, createElement } from "react";
+import { View, TouchableOpacity } from 'react-native';
+
+import Collapsible from 'react-native-collapsible';
+import { HelloWorldSample } from "./components/HelloWorldSample";
+
+export class CollapsibleContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isCollapsed: true
+        }
+
+        this.toggleContent = this.toggleContent.bind(this);
+    }
+
+    toggleContent() {
+        this.setState({ isCollapsed: !this.state.isCollapsed });
+    }
+
+    render() {
+        const { headerCollapsed, headerOpen, content, duration } = this.props;
+        return (
+            <View>
+                <TouchableOpacity onPress={this.toggleContent}>
+                    {this.state.isCollapsed ? headerCollapsed : headerOpen}
+                </TouchableOpacity>
+                <Collapsible collapsed={this.state.isCollapsed} duration={duration}>
+                    {content}
+                </Collapsible>
+            </View>
+        )
+    }
+}
